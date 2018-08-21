901d83da912552047126d6c9a0c3b2f66706f4cb
jonathan_geovany <hv12i04001@usonsonate.edu.sv>
2018-08-20 20:24:48 -0600

    pruebas de ik y fk para demostrar resultados

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,20 @@ import com.sun.tools.internal.ws.wsdl.document.Input;
 import gui.Window;
 import gui.frmArmController;
 import kinematics.ForwardK;
+import kinematics.InverseK;
+import neuralNet.ApplyMLP;
+import neuralNet.mlp.transferfunctions.SigmoidalTransfer;
 import processing.core.PApplet;
 import utils.PRECISION;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("\nPruebas de cinematica\n\n");
 
         Main m = new Main();
 
@@ -20,14 +25,34 @@ public class Main {
         //PApplet.main(pWindow.getClass());
 
 
-
-
-
         double L[] = new double[]{28,50,60};
-        neuralNet.Input input = new neuralNet.Input(L, PRECISION.LOW,false,0,90,-90,90);
+        double coords[] = {110,0,28};
+        double angles[] = {0,0,0};
+
+        ForwardK fk = new ForwardK(L);
+        InverseK ik = new InverseK(L);
+
+        System.out.println("Longitudes de piezas    : "+Arrays.toString(L));
+        System.out.println("Pruebas de cinematica directa");
+        System.out.println("Angulos de entrada      : "+Arrays.toString(angles));
+        System.out.println("Coordenadas de salida   : "+Arrays.toString(fk.getCartesian(angles,false)));
+        System.out.println("Pruebas de cinematica inversa");
+        System.out.println("Coordenadas de entrada  : "+Arrays.toString(coords));
+        System.out.println("Angulos de salida       : "+Arrays.toString(ik.getAngles(coords)));
+
+
+
+
+        //neuralNet.Input input = new neuralNet.Input(L, PRECISION.LOW,false,0,90,-90,90);
         //input.getInputs();
-        input.saveInputs(input.normalizeInputs(input.getInputs()),"pruebas2018.txt");
+        //input.saveInputs(input.getInputs(),"pruebas2018.txt");
         //input
+
+        //ArrayList<double[][]> inputs = input.normalizeInputs(input.getInputs());
+        //input.saveInputs(inputs,"pruebas2018.txt");
+        //ApplyMLP applyMLP = new ApplyMLP(new int[]{2,10,2},0.6,new SigmoidalTransfer());
+
+        //applyMLP.train(8000,0.6,inputs);
 
         //1622699
 
